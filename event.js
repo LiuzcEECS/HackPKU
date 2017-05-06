ec5cf65a778c507079d3c3950bbc195480ec1dde
LiuzcEECS <liuzceecs@gmail.com>
2017-05-06 22:54:43 +0800

    add anime

--- event.js
+++ event.js
@@ -1,11 +1,28 @@
 
 var canvasW;
 var canvasH;
+var wavesDiv = document.getElementById('waves-effect');
 
 //initialization
 function init(){
 	resize();
 	getBeatmap("Alan Walker - Faded (Astarte) [Turtle's Standard].osu");
+}
+
+function anime(){
+	wavesDiv.className = 'waves-effect';
+    var wH = 500;
+    var iX = e.pageX - box.offset().left;
+    var iY = e.pageY - box.offset().top;
+    var nX = iX - wH/2;
+    var nY = iY - wH/2;
+
+    wavesDiv.css({
+        width: wH,
+        height: wH,
+        left: nX,
+        top: nY
+    }).addClass("waves-effect-animation");
 }
 
 function resize(){
