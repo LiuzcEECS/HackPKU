7890e1ad753b05e60fb30d9ebf7275548665c19d
Iraka <iraka@pku.edu.cn>
2017-05-06 22:42:40 +0800

    slide judgement

--- event.js
+++ event.js
@@ -14,6 +14,7 @@ function resize(){
 	$("#game").attr({"width":canvasW,"height":canvasH});
 	beginRadius = canvasW/6;
 	defaultRadius = canvasW/30;
+	judgeSlideRadius = defaultRadius*2;
 }
 
 function getBeatmap(filename){
--- judgement.js
+++ judgement.js
@@ -1,4 +1,5 @@
 var judgeInterval=200;
+var judgeSlideRadius=300;
 player.performance={
 	score:0,
 	combo:0,
@@ -13,17 +14,14 @@ function onHandGrasp(x,y){
 	var deltaT=datanow[2]-timenow;
 
 	console.log("deltaT="+deltaT);
-	if(deltaT>=200)return; // not considered as clicking this yet
-	if(deltaT<=-200)return; // handled by render timer
+	if(deltaT>=judgeInterval)return; // not considered as clicking this yet
+	if(deltaT<=-judgeInterval)return; // handled by render timer
 
 	var sqrDist=(x-datanow[0])*(x-datanow[0])+(y-datanow[1])*(y-datanow[1]);
 	if(sqrDist>defaultRadius*defaultRadius)return; // out of boundary
 
-	beatmap.nextbeat += 1;
-	player.performance.combo++;
-	if(player.performance.maxCombo<player.performance.combo)
-		player.performance.maxCombo=player.performance.combo;
-	console.log("HP="+player.performance.hp+" Combo="+player.performance.combo);
+	if(datanow.length==3) // is a single hit
+		beatHit(); // already hit
 }
 
 function onHandRelease(x,y){
@@ -37,11 +35,45 @@ function beatMissed(){
 	player.performance.combo=0;
 }
 
+function beatHit(){
+	console.log("Hit");
+	console.log("HP="+player.performance.hp+" Combo="+player.performance.combo);
+	beatmap.nextbeat+=1;
+	player.performance.combo++;
+	if(player.performance.maxCombo<player.performance.combo)
+		player.performance.maxCombo=player.performance.combo;
+}
+
+function judgeSlide(time,prX,prY){
+	var datanow=beatmap.datalist[beatmap.nextbeat];
+	if(datanow.length==3)return;
+
+	if(time>datanow[6]){ // no error to the end
+		beatHit();
+	}
+	else{ // still sliding
+		var sqrDist=(prX-player.x)*(prX-player.x)+(prY-player.y)*(prY-player.y);
+		if(sqrDist<=judgeSlideRadius*judgeSlideRadius){ // in safe range
+			return; // continue;
+		}
+		else{ // out of slide range
+			beatMissed();
+		}
+	}
+}
+
 //================================= Caller =============================
 function onMouseDown(event){
+	player.grabbed=true;
 	onHandGrasp(event.clientX,event.clientY);
 }
 
 function onMouseUp(event){
+	player.grabbed=false;
 	onHandRelease(event.clientX,event.clientY);
+}
+
+function onMouseMove(event){
+	player.x=event.clientX;
+	player.y=event.clientY;
 }
